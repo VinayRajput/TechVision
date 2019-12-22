1d265652e95a51a30e098312e916c20b35b62f37
Vinay Rajput <vinkrins@gmail.com>
2019-12-23 00:00:49 +0530

    Save function added and worked successfully

--- Courselect/server/controller/topic.controller.js
+++ Courselect/server/controller/topic.controller.js
@@ -1,35 +1,43 @@
 const topicModel = require('../model/topic');
 const db = require('../utilities/dbConnect');
-const table = new db({tableName:"topic"});
-exports.add = function(req, res){
-   const record = table({
-      name:"Record Sample",
-      
-   })
-   res.json({"msg":"add Topic Called"})
+const table = db.extend({
+   tableName: "topic",
+});
+exports.add = function (req, res) {
+   record = new table({
+      name: "Record Sample",
+      price: 399.99,
+      length: "30:00",
+      weightage: 20
+   });
+   console.log(record);
+   record.save(function (err, row) {
+      res.json(row);
+   });
+
 }
 
-exports.getAllTopic = function(req, res){
+exports.getAllTopic = function (req, res) {
    res.header("Access-Control-Allow-Origin", "*");
-   table.find('all',function(err,row,fields){
-      if(err){
-         res.json({"msg":err});
+   table.find('all', function (err, row, fields) {
+      if (err) {
+         res.json({ "msg": err });
          return;
       }
-     res.json(row);
+      res.json(row);
    })
 }
 
-exports.getTopicById = function(req,res){
-   table.find('all',{where:`id=${req.params.id}`},function(err, row){
+exports.getTopicById = function (req, res) {
+   table.find('all', { where: `id=${req.params.id}` }, function (err, row) {
       res.json(row);
-   }) 
+   })
 }
 
-exports.update = function(req,res){
-   res.json({msg:"update Topic called"});
+exports.update = function (req, res) {
+   res.json({ msg: "update Topic called" });
 }
 
-exports.delete = function(req,res){
-   res.json({msg:"delete Topic called"});
+exports.delete = function (req, res) {
+   res.json({ msg: "delete Topic called" });
 }
