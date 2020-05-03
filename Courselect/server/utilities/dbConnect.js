3c7f57639114253285ddb2bcbe851fb91b404926
Vinay Rajput <vinkrins@gmail.com>
2020-05-03 23:26:21 +0530

    Removed extra other files

--- Courselect/server/utilities/dbConnect.js
+++
@@ -1,6 +0,0 @@
-var mysqlModel = require('mysql-model');
-var config = require('../configs/config.json')
-const dbConfig = config.dbConfig;
-var db = mysqlModel.createConnection(dbConfig);
-/* exports.dbFind = function(tableName, ){}*/
- module.exports = db;
