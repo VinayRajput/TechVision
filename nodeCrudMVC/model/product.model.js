26ea3fe62a23c5c9d0e5f6894a3e3968286666a2
Vinay Rajput <vinkrins@gmail.com>
2020-05-03 23:24:36 +0530

    Removed extra other files

--- nodeCrudMVC/model/product.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
-
-let Product = new Schema({
-   name: { required: true, primary: true, type:String },
-   qty: { require: true, type:Number }
-})
-
-module.exports = mongoose.model('Product',Product);
